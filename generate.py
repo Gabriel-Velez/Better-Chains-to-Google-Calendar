1153d1fbbbfaf41b8775236a828322c8b4cb8a66
gabriel velez <gabriel.dan.velez@gmail.com>
2025-04-12 14:47:00 -0400

    updated generate.py

--- generate.py
+++ generate.py
@@ -1,3 +1,4 @@
+import os
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
@@ -9,6 +10,6 @@ def authenticate_google():
     creds = flow.run_local_server(port=0)
     with open('token.json', 'w') as token:
         token.write(creds.to_json())
-    print("Authentication complete!")
+    print("Authentication complete! token.json saved.")
 
 authenticate_google()
